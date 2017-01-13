9c63b9aef1718d621b3193e006cf02cf4f695cac
Michael Söderman <micke@devcube.se>
2017-01-13 22:50:51 +0100

    Trying to add autoprefixer support

--- skeleton-typescript-webpack/webpack.config.ts
+++ skeleton-typescript-webpack/webpack.config.ts
@@ -21,6 +21,9 @@ import * as copyFiles from '@easy-webpack/config-copy-files';
 import * as uglify from '@easy-webpack/config-uglify';
 import * as generateCoverage from '@easy-webpack/config-test-coverage-istanbul';
 
+var loaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+var autoprefixer = require('autoprefixer');
+
 const ENV: 'development' | 'production' | 'test' = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() || (process.env.NODE_ENV = 'development');
 
 // basic configuration:
@@ -79,24 +82,37 @@ let config = generateConfig(
     output: {
       path: outDir,
     },
+    plugins: [
+      new loaderOptionsPlugin({
+        test: /\.css$/,
+        options: {
+            postcss: [
+                autoprefixer({
+                    browsers: ['last 2 version']
+                })
+            ]
+        }
+      })
+    ]
   },
 
   /**
    * Don't be afraid, you can put bits of standard Webpack configuration here
    * (or at the end, after the last parameter, so it won't get overwritten by the presets)
    * Because that's all easy-webpack configs are - snippets of premade, maintained configuration parts!
-   * 
+   *
    * For Webpack docs, see: https://webpack.js.org/configuration/
    */
 
-  ENV === 'test' || ENV === 'development' ? 
+  ENV === 'test' || ENV === 'development' ?
     envDev(ENV !== 'test' ? {} : {devtool: 'inline-source-map'}) :
     envProd({ /* devtool: '...' */ }),
 
   aurelia({root: rootDir, src: srcDir, title: title, baseUrl: baseUrl}),
   typescript(ENV !== 'test' ? {} : { options: { doTypeCheck: false, sourceMap: false, inlineSourceMap: true, inlineSources: true } }),
   html(),
-  css({ filename: 'styles.css', allChunks: true, sourceMap: false }),
+  css({ filename: 'styles.css', allChunks: true, sourceMap: false,
+        additionalLoaders: ['postcss'] }),
   fontAndImages(),
   globalBluebird(),
   globalJquery(),
